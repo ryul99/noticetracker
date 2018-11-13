601c4cd4fb007e434c0162c76ebf958607ef0784
minty99 <mhkim4886@gmail.com>
2018-11-14 04:25:41 +0900

    Frontend: CourseService implemented

--- frontend/src/app/course.service.ts
+++ frontend/src/app/course.service.ts
@@ -1,21 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Course } from './course';
 import { Site } from './site';
 import { Observable, of } from 'rxjs';
-import { Course } from './course';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  searchByCode(courseCode: string): Observable<Course[]> {}
+  searchByCode(courseCode: string): Observable<Course[]> {
+    let url = 'api/search/id/' + courseCode;
+    return this.http.get<Course[]>(url);
+  }
 
-  searchByName(name: string): Observable<Course[]> {}
+  searchByName(courseName: string): Observable<Course[]> {
+    let url = 'api/search/name/' + courseName;
+    return this.http.get<Course[]>(url);
+  }
 
-  getCourseObjectById(courseId: number): Observable<Course> {}
+  getCourseObjectById(courseId: number): Observable<Course> {
+    let url = 'api/course/' + courseId;
+    return this.http.get<Course>(url);
+  }
 
-  getRecommendedSitesById(courseId: number): Observable<Site[]> {}
+  getRecommendedSitesById(courseId: number): Observable<Site[]> {
+    let url = 'api/course/' + courseId + '/site';
+    return this.http.get<Site[]>(url);
+  }
 
-  addSiteByCourseId(courseId: number, site: Site): Observable<Site> {}
+  addSiteByCourseId(courseId: number, site: Site): Observable<Site> {
+    let url = 'api/course/' + courseId + '/site';
+    return this.http.post<Site>(url, site);
+  }
 }
