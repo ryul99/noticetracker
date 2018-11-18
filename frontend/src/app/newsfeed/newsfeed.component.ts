935aea8f12ae50b39c4e0ebb9fe2e9138b3afebf
2017-10332 <riman314@snu.ac.kr>
2018-11-19 05:34:19 +0900

    Frontend: star and ignore in newsfeedComponent is implemented with mock-data

--- frontend/src/app/newsfeed/newsfeed.component.ts
+++ frontend/src/app/newsfeed/newsfeed.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../user.service';
+import { Article } from '../article';
+import { Course } from '../course';
+import { mockCourses } from '../stub';
 
 @Component({
   selector: 'app-newsfeed',
@@ -8,6 +11,19 @@ import { UserService } from '../user.service';
   styleUrls: ['./newsfeed.component.css']
 })
 export class NewsfeedComponent implements OnInit {
+  timeTable: Course[] = mockCourses;
+  newsfeed: Article[] = [
+    { id: 1, url: 'https://www.naver.com/' },
+    { id: 2, url: 'https://github.com/' },
+    { id: 3, url: 'https://www.youtube.com/' },
+    { id: 4, url: 'https://www.daum.net/' },
+    { id: 5, url: 'https://www.google.com/' },
+    { id: 6, url: 'https://stackoverflow.com/' }
+  ];
+  courseIdOfArticle: number[] = [1, 2, 3, 4, 5, 6];
+  articleIsStar: boolean[] = [false, false, false, false, false, false];
+  articleIsIgnore: boolean[] = [false, false, false, false, false, false];
+
   constructor(private router: Router, private userService: UserService) {}
 
   ngOnInit() {}
@@ -23,5 +39,17 @@ export class NewsfeedComponent implements OnInit {
   signOut() {
     this.userService.signOut();
     this.router.navigate(['']);
+  }
+
+  star(article: Article) {
+    let index = this.newsfeed.indexOf(article);
+    this.articleIsStar[index] = true;
+    this.articleIsIgnore[index] = false;
+  }
+
+  ignore(article: Article) {
+    let index = this.newsfeed.indexOf(article);
+    this.articleIsStar[index] = false;
+    this.articleIsIgnore[index] = true;
   }
 }
