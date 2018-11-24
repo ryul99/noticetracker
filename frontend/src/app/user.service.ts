e41550441a6798451146af82116502525157034d
2017-10332 <riman314@snu.ac.kr>
2018-11-24 18:05:21 +0900

    Frontend: signIn, signUp, signOut method in user service

--- frontend/src/app/user.service.ts
+++ frontend/src/app/user.service.ts
@@ -2,31 +2,69 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Course } from './course';
 import { Site } from './site';
+import { User } from './user';
 import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  userId: string;
+  userNumber: number;
   username: string;
 
   constructor(private http: HttpClient) {}
 
-  signIn(userId: string, password: string) {}
+  //signIn(userId: string, password: string) {}
 
-  signOut() {}
+  signIn(userId: string, password: string): Promise<boolean> {
+    return this.http
+      .post<User>('/api/sign_in/', {
+        email: userId,
+        password: password
+      })
+      .toPromise()
+      .then(
+        user => {
+          this.username = user.username;
+          this.userNumber = user.id;
+          return true;
+        },
+        error => {
+          return false;
+        }
+      );
+  }
 
-  signUp(userId: string, password: string) {}
+  signOut(): Promise<boolean> {
+    return this.http.get<boolean>('/api/sign_out/').toPromise();
+  }
 
+  signUp(userId: string, password: string): Promise<boolean> {
+    return this.http
+      .post<User>('/api/sign_up/', {
+        email: userId,
+        password: password
+      })
+      .toPromise()
+      .then(
+        user => {
+          this.username = user.username;
+          this.userNumber = user.id;
+          return true;
+        },
+        error => {
+          return false;
+        }
+      );
+  }
   addCourse(courseId: number) {}
 
   getTakingCourses(): Observable<Course[]> {
-    return this.http.get<Course[]>('/api/user/' + this.userId + '/courses');
+    return this.http.get<Course[]>('/api/user/' + this.userNumber + '/courses');
   }
 
-  getUserId(): string {
-    return this.userId;
+  getUserId(): number {
+    return this.userNumber;
   }
 
   getUserName(): string {
