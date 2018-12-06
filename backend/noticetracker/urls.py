5d38f4ae10bb650919a9549ae8314515b2f9f947
minty99 <mhkim4886@gmail.com>
2018-12-06 17:19:22 +0900

    Backend: update urls.py

--- backend/noticetracker/urls.py
+++ backend/noticetracker/urls.py
@@ -2,20 +2,20 @@ from django.urls import path
 from noticetracker import views
 
 urlpatterns = [
-    path('sign_in', views.signin, name='signin'),
-    path('sign_up', views.signup, name='signup'),
-    path('sign_out', views.signout, name='signout'),
-    path('search/name/<str:courseName>',
+    path('sign_in/', views.signin, name='signin'),
+    path('sign_up/', views.signup, name='signup'),
+    path('sign_out/', views.signout, name='signout'),
+    path('search/name/<str:courseName>/',
          views.searchByName, name='searchByName'),
-    path('search/code/<str:courseCode>',
+    path('search/code/<str:courseCode>/',
          views.searchByCode, name='searchByCode'),
-    path('course/<int:courseId>', views.courseDetail, name='courseDetail'),
-    path('course/<int:courseId>/site',
+    path('course/<int:courseId>/', views.courseDetail, name='courseDetail'),
+    path('course/<int:courseId>/site/',
          views.sitesByCourseId, name='sitesByCourseId'),
-    path('user/course', views.userCourse, name='userCourse'),
-    path('user/course/<int: courseId>/article/<int:pageId>',
+    path('user/course/', views.userCourse, name='userCourse'),
+    path('user/course/<int: courseId>/article/',
          views.courseArticles, name='courseArticles'),
-    path('user/newsfeed/<int:pageId>', views.newsfeedPage, name='newsfeedPage'),
-    path('user/article/<int:articleId>',
+    path('user/newsfeed/', views.newsfeedPage, name='newsfeedPage'),
+    path('user/article/<int:articleId>/',
          views.userArticle, name='userArticle')
 ]
