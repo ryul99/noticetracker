1f77c6db42e01432bb649cdc74cb909438c2843e
Changmin Choi <cmchoi9901@gmail.com>
2018-12-12 03:30:26 +0900

    Backend: debug baseCrawl.py and refactoring baseCrawl.py

--- backend/noticetracker/baseCrawl.py
+++ backend/noticetracker/baseCrawl.py
@@ -1,4 +1,6 @@
-# from .models import LectureTime, Site, Course, CourseCustom, Article, UserDetail
+# from .models import LectureTime, Site, Course, CourseCustom, Article, UserDetail, SiteHref
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
 import requests
 import csv
 from urllib.request import urlopen
@@ -6,15 +8,33 @@ from bs4 import BeautifulSoup
 import re
 
 
+def rawHref2Url(href, root, slicedUrl):
+    ret = href
+    val = URLValidator()
+    try:
+        val(href)
+    except ValidationError:
+        if href[0] == "/":
+            ret = root + href
+        else:
+            # remove last elements
+            r = "/".join([i[1] for i in slicedUrl[0:len(slicedUrl) - 1]])
+            ret = r + '/' + href
+    return ret
+
+
 def crawl(url, site):
     req = requests.get(url)
-    if req.status_code == 200 & & req.ok:
+    if req.status_code == 200 and req.ok:
+        slicedUrl = re.findall('([^/]+[/]{2})?([^/]+)', url)
+        root = slicedUrl[0][1]  # e.g. stackoverflow.com
         bsObject = BeautifulSoup(req.text, "html.parser")
         hyperLinks = bsObject.find_all("a")
         for hyperLink in hyperLinks:
             href = hyperLink.get('href')
             if href == None:
                 continue
+            href = rawHref2Url(href, root, slicedUrl)
             siteHref = SiteHref(href=href, site=site)
             siteHref.save()
     else:
