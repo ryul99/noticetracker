c6df889b2b36cbb20c53f51bb7f4459cb85bb69b
Changmin Choi <cmchoi9901@gmail.com>
2018-12-17 15:31:22 +0900

    Backend: make scan get course

--- backend/noticetracker/allTimeSiteCrawl.py
+++ backend/noticetracker/allTimeSiteCrawl.py
@@ -9,7 +9,7 @@ import re
 from baseCrawl import rawHref2Url, crawl
 
 
-def scan(site):
+def scan(site, course):
     url = site.url
     hrefs = crawl(url)
     addedHrefs = list()
@@ -17,11 +17,11 @@ def scan(site):
         searchedHref = site.hrefs.filter(href=href)
         if searchedHref == None:
             addedHrefs.append(href)
-    for href in site.hrefs.all():  # need to test
-        if href.href not in hrefs:
-            href.delete()
+    # for href in site.hrefs.all():  # need to test
+    #     if href.href not in hrefs:
+    #         href.delete()
     for href in addedHrefs:
-        article = Article(url=href, fromSite=site)  # need to fill fromCourse
+        article = Article(url=href, fromSite=site, fromCourse=course)
         article.save()
         sitehref = SiteHref(href=href, site=site)
         sitehref.save()
