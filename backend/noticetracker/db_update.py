c82dd5abfa277ee386ec0df0e86e7d61569a79a7
minty99 <mhkim4886@gmail.com>
2018-12-17 19:46:47 +0900

    Hotfix: fix type error

--- backend/noticetracker/db_update.py
+++ backend/noticetracker/db_update.py
@@ -7,12 +7,13 @@ import time
 from datetime import datetime, timezone
 from .crawl_diff import scan
 
+
 def update_auto():
     while True:
         print("Update start: " + str(datetime.now(timezone.utc)))
         update()
         time.sleep(15)
-        
+
 
 def update():
     users = list(UserDetail.objects.all())
@@ -21,4 +22,4 @@ def update():
         for course in courses:
             sites = list(course.siteList.all())
             for site in sites:
-                scan(site, course)
+                scan(site, course.course)
